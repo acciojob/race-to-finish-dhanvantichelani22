3bcffb1416f590bc4d51ac6f30271c481c2b5da3
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-06-11 17:16:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 window.promises = [];
-
+https://github.com/acciojob/race-to-finish-Nitintembhare29/blob/master/script.js
 // Do not change the code above this
 // add your promises to the array `promises`
